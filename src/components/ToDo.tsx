a7c34c309308e04381486b32053eaa6212188805
KKamjangE <ajm980718@gmail.com>
2023-02-12 16:29:32 +0900

    feat: ToDo category 상태 변경 구현

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,36 @@
-import { IToDo } from "./atoms";
+import { IToDo, toDoState } from "./atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldTodos) => {
+      return oldTodos.map((toDo) =>
+        toDo.id === id ? { ...toDo, category: name as IToDo["category"] } : toDo
+      );
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Done</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
